2b400a4355934ff9a6bba3ce51801ee4dec3a923
michael-klidzhan <johniehuge@gmail.com>
2025-05-30 23:02:29 +0300

    create view for upload_excel

--- xlimport/xlimportapp/views.py
+++ xlimport/xlimportapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpRequest
-from .models import Album
+from .models import Album, Customer, SiteObject
 import pandas as pd
 
 
@@ -25,7 +25,31 @@ def get_albums(request: HttpRequest):
 
     return JsonResponse(albums_list, safe=False)
 
+def upload_excel(request: HttpRequest):
 
+    if request.method == "POST" and request.FILES.get("excel_file"):
+        try:
+            df = pd.read_excel(request.FILES["excel_file"])
+
+            for _, row in df.iterrows():
+                customer, _ = Customer.objects.get_or_create(name=row["Заказчик"])
+                site_object, _ = SiteObject.objects.get_or_create(name=row["Объект"])
+
+                Album.objects.create(
+                    customer=customer,
+                    site_object=site_object,
+                    documentation_type=row["Вид документации"],
+                    volume=row["Объем"],
+                    name=row["Наименование"],
+                    file_name=row["Название файла"],
+                )
+
+            return JsonResponse({"status": "success",})
+        
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+        
+    return JsonResponse({"error": "Invalid request"}, status=400)
 
 
 
