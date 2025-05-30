171ff939ce5d80ac91ca9462ebfb9f0833504dec
michael-klidzhan <johniehuge@gmail.com>
2025-05-30 23:05:45 +0300

    update xlimportapp urls.py

--- xlimport/xlimportapp/urls.py
+++ xlimport/xlimportapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ImportIndexView, UploadExcelTableView
+from .views import index, get_albums, upload_excel
 
 app_name = "xlimportapp"
 
 urlpatterns = [
-    path("", ImportIndexView.as_view(), name="index"),
-    path("upload/", UploadExcelTableView.as_view(), name="upload")
+    path("", index, name="index"),
+    path("get-albums/", get_albums, name="get_albums"),
+    path("upload/", upload_excel, name="upload_excel"),
 ]
